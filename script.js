e9ce50e895808da25e676930446f3014621db567
thetopaccount <thetopaccount@protonmail.com>
2022-01-22 12:54:06 +0530

    Write function for playing game of 5 rounds; refactor code

--- script.js
+++ script.js
@@ -19,23 +19,9 @@ const computerPlay = () => {
 }
 
 
-// Messages for possible result combinations:
-const drawResult = "It's an even fight - neither won.";
-const rockVsPaperResult = "Tough luck, you lose - paper eats rock :(";
-const rockVsScissorsResult = "Yay, you win - rock crushes scissors!";
-const paperVsRockResult = "Yay, you win - paper eats rock!";
-const paperVsScissorsResult = "Tough luck, you lose - scissors cut paper :(";
-const scissorsVsRockResult = "Tough luck, you lose - rock crushes scissors :(";
-const scissorsVsPaperResult = "Yay, you win - scissors cut paper!";
-
-// Message for wrong string entered:
-const unrecognizedSelection = "Sorry! We do not recognize the object you entered.\nPlease enter either 'rock', 'paper', or 'scissors'.";
-
-
 function playRound (playerSelection, computerSelection) {
-
 	// Make player's selection case-insensitive first:
-	playerSelection = playerSelection.toLowerCase();
+	playerSelection ? playerSelection = playerSelection.toLowerCase() : '';
 
 	// Check if it's a draw first:
 	if (playerSelection === computerSelection) {
@@ -48,12 +34,15 @@ function playRound (playerSelection, computerSelection) {
 		switch (playerSelection) {
 			case "rock":
 				computerSelection === "paper" ? finalResult = rockVsPaperResult : finalResult = rockVsScissorsResult;
+				finalResult === rockVsPaperResult ? computerScore++ : playerScore++;
 				break;
 			case "paper":
 				computerSelection === "rock" ? finalResult = paperVsRockResult : finalResult = paperVsScissorsResult;
+				finalResult === paperVsRockResult ? playerScore++ : computerScore++;
 				break;
 			case "scissors":
 				computerSelection === "rock" ? finalResult = scissorsVsRockResult : finalResult = scissorsVsPaperResult;
+				finalResult === scissorsVsRockResult ? computerScore++ : playerScore++;
 				break;
 			default:
 				return unrecognizedSelection;
@@ -63,6 +52,51 @@ function playRound (playerSelection, computerSelection) {
 }
 
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+	let playerSelection;
+	let computerSelection;
+	let isGameWon = false;
+	let currentRound = 1;
+
+	// Setting the stage...
+	console.clear();
+
+	while (!isGameWon) {
+		playerSelection = prompt("What's your weapon of choice? (Rock/Paper/Scissors)");
+		computerSelection = computerPlay();
+
+		console.group(`Round ${currentRound}`);
+		console.log(playRound(playerSelection, computerSelection));
+		console.log(`YOU ${playerScore} - ${computerScore} COM`);
+		console.groupEnd();
+
+		// First to win 5 rounds wins the game
+		(playerScore === 5 || computerScore === 5) ? isGameWon = true : isGameWon = false;
+
+		isGameWon && playerScore === 5 ? console.log("\nWe have a winner (and it's YOU)!\n ") : 
+			isGameWon && computerScore === 5 ? console.error("Better luck next time...") : 
+				isGameWon = false;
+
+		currentRound++;
+	}
+}
+
+
+// Global vars for keeping track of the score
+let playerScore = 0;
+let computerScore = 0;
+
+
+// Messages for possible results:
+const unrecognizedSelection = "Sorry! We do not recognize the object you entered. Please enter either 'rock', 'paper', or 'scissors'.";
+const drawResult = "It's an even fight - neither won.";
+const rockVsPaperResult = "Tough luck, you lose - paper eats rock :(";
+const rockVsScissorsResult = "Yay, you win - rock crushes scissors!";
+const paperVsRockResult = "Yay, you win - paper eats rock!";
+const paperVsScissorsResult = "Tough luck, you lose - scissors cut paper :(";
+const scissorsVsRockResult = "Tough luck, you lose - rock crushes scissors :(";
+const scissorsVsPaperResult = "Yay, you win - scissors cut paper!";
+
+
+// Play a game:
+game();
